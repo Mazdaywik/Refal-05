ec9ba66fcf7673e7c0a182bf1706597d7ee5352e
Mazdaywik <a.v.konovalov87@mail.ru>
2009-07-18 22:19:42 +0300

    Simple Refal.004

--- Compiler/makeself-s.bat
+++ Compiler/makeself-s.bat
@@ -4,8 +4,6 @@
 @set modules=%modules% Error.sref Generator.sref SymTable.sref
 @set modules=%modules% FindFile ParseCmdLine Context.sref
 @set modules=%modules% Library LibraryEx refalrts.cpp
-@srefc_ -c "vs cl /EHcs /W3 /wd4996 /wd4127 /wd4702 -I..\SRLib -DINTERPRET1" -d ..\SRLib %modules%
-@rem srefc_ -c "compile_bcc -I..\SRLib -DINTERPRET1 " -d ..\SRLib %modules%
-@rem srefc_ -c "g++ -Wall -I..\SRLib -DINTERPRET1 -osrefc.exe " -d ..\SRLib %modules%
+@srefc.s -c "compile_bcc -I..\SRLib -w-aus" -d ..\SRLib %modules%
 @del *.obj
 @del *.tds
