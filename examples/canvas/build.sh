69e457dbd57efe7404fb102aba770c22e8ecb9d9
Mihail Butvin <butvin.mihail@yandex.ru>
2024-11-11 06:30:36 +0300

    Export malloc

--- examples/canvas/build.sh
+++ examples/canvas/build.sh
@@ -1,3 +1,3 @@
 export R05ROOT="/home/butvinm/Dev/lame-refal"
 export R05PATH="$R05ROOT/lib:$R05ROOT/src:$R05ROOT/examples/canvas"
-R05CCOMP="clang -DLAMELIB -target wasm32 -ffreestanding --no-standard-libraries -fno-builtin -Wl,--export-table -Wl,--no-entry -Wl,--export=main -Wl,--allow-undefined -o main.wasm" $R05ROOT/bin/refal05c main refal05rts Library Canvas
+R05CCOMP="clang -DLAMELIB -target wasm32 -ffreestanding --no-standard-libraries -fno-builtin -Wl,--export-table -Wl,--no-entry -Wl,--export=main -Wl,--export=malloc -Wl,--allow-undefined -o main.wasm" $R05ROOT/bin/refal05c main refal05rts Library Canvas
